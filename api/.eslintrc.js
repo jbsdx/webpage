c3e3e00dd3292e5b96c49ff60c5c00b62dde418f
Jonas Benz <jonasbenz@gmail.com>
2019-08-05 19:10:14 +0200

    feat(authorization): added permission keys

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,3 +1,8 @@
+const path = require('path');
 module.exports = {
   extends: '@loopback/eslint-config',
+  parserOptions: {
+    project: path.resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
+  },
 };
