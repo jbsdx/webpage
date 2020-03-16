b7c0559f69ef47cd559bee5683c79b0a0c6d3b51
Jonas Benz <dev@jbnz.de>
2020-03-17 00:38:29 +0100

    build: updated docker stack

--- api/src/migrations/mongo-init.js
+++ api/src/migrations/mongo-init.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
-db.auth('admin-user', 'dk48d1JdkvnDK38zjnDK1jvkla');
+db.auth('admin-user', 'akl32kljkdg82lja892JDaf82Jv');
 
 db = db.getSiblingDB('webpage');
 
 db.createUser({
   user: 'webpage-user',
   pwd: 'dkjJd8hbJdk81hCkf731',
-  roles: [{ role: 'readWrite', db: 'webpage', }]
+  roles: [{role: 'readWrite', db: 'webpage'}],
 });
 
 print('init mongodb..');
